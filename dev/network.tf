a3f8d4f47f79bd7b898b8033d7990a53b8cab570
prathyaksh <prathyaksh.ps@gmail.com>
2026-01-03 20:59:20 +0530

    removed comments

--- dev/network.tf
+++ dev/network.tf
@@ -1,5 +1,4 @@
 module "vpc_dev" {
-  # For now, we use a relative path. Later, we can switch to Git Tags.
   source = "../../ps-ledger-infra-modules-tf/modules/vpc"
 
   network_name = var.dev_vpc_name
