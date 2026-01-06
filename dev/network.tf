3255043bdd4f20328f873bf2c05dd3c64476dd56
prathyaksh <prathyaksh.ps@gmail.com>
2026-01-06 18:12:59 +0530

    Fixing the NAT and firewall

---
+++ dev/network.tf
@@ -0,0 +1,18 @@
+module "vpc_dev" {
+  source       = "../../ps-ledger-infra-modules-tf/modules/vpc"
+  network_name = var.dev_vpc_name
+  region       = var.region
+  cidr_range   = var.dev_cidr_range
+}
+
+module "nat" {
+  source       = "../../ps-ledger-infra-modules-tf/modules/nat"
+  network_name = var.dev_vpc_name
+  region       = var.region
+  network_id   = module.vpc_dev.network_id
+}
+
+module "firewall_rules_dev" {
+  source       = "../../ps-ledger-infra-modules-tf/modules/firewall"
+  network_name = var.dev_vpc_name
+}
--- dev/test_nat.tf
+++
@@ -1,6 +0,0 @@
-module "nat" {
-    source       = "../../ps-ledger-infra-modules-tf/modules/nat"
-    network_name = var.dev_vpc_name
-    region       = var.region
-    network_id   = module.vpc_dev.network_id
-}
