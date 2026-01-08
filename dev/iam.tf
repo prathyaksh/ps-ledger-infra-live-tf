62a12fc85a957306a8938b32bd4a33e6574dab1c
prathyaksh <prathyaksh.ps@gmail.com>
2026-01-09 01:16:28 +0530

    modifying IAm

--- dev/iam.tf
+++ dev/iam.tf
@@ -1,6 +1,30 @@
-module "finance_ledger_sa" {
+# 1. JUST create a Service Account
+module "create_ledger_sa" {
   source       = "../../ps-ledger-infra-modules-tf/modules/iam"
-  sa_id = var.sa_id
-  project_id = var.project_id
+  project_id   = var.project_id
+  create_sa    = true
+  sa_id        = "finance-ledger-sa"
+  display_name = "Finance App SA"
+}
 
+# 2. JUST add roles to the SA (Independent call)
+module "assign_sa_roles" {
+  source     = "../../ps-ledger-infra-modules-tf/modules/iam"
+  project_id = var.project_id
+  member_id  = "serviceAccount:finance-ledger-sa@${var.project_id}.iam.gserviceaccount.com"
+  roles_list = [
+    "roles/logging.logWriter",
+    "roles/monitoring.metricWriter"
+  ]
+}
+
+# 3. JUST add roles to YOUR human ID (Independent call)
+module "assign_my_user_roles" {
+  source     = "../../ps-ledger-infra-modules-tf/modules/iam"
+  project_id = var.project_id
+  member_id  = "user:prathyaksh.ps@gmail.com"
+  roles_list = [
+    "roles/iap.tunnelResourceAccessor",
+    "roles/compute.viewer"
+  ]
 }
