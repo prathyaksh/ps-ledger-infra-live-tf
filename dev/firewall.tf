e7e89d163483273c319eef33f4774eafe6f79ae3
prathyaksh <prathyaksh.ps@gmail.com>
2026-01-10 21:45:07 +0530

    updating firewall

--- dev/firewall.tf
+++ dev/firewall.tf
@@ -3,27 +3,18 @@ module "firewall_rules" {
   network_name = module.vpc_dev.network_name
 
   firewall_rules = {
-    # Rule 1: SECURE SSH via IAP (Only Google's Proxy Range)
     "allow-iap-ssh" = {
-      allow = [
-        {
-         protocol = "tcp",
-         ports = ["22"] 
-         }
-         ]
-      source_ranges = ["35.235.240.0/20"] # Google's IAP Range
+      action        = "allow"        # Added this
+      protocol      = "tcp"
+      ports         = ["22"]
+      source_ranges = ["35.235.240.0/20"]
       target_tags   = ["ssh-access"]
     },
-    
-    # Rule 2: PUBLIC HTTP (For our Nginx Landing Page)
     "allow-http-web" = {
-      allow = [
-        {
-         protocol = "tcp",
-         ports = ["80"] 
-         }
-         ]
-      source_ranges = ["0.0.0.0/0"] # The Internet
+      action        = "allow"        # Added this
+      protocol      = "tcp"
+      ports         = ["80"]
+      source_ranges = ["0.0.0.0/0"]
       target_tags   = ["http-server"]
     }
   }
