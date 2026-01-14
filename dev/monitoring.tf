e413870dba1248f30596f056c4e55f353556a235
prathyaksh <prathyaksh.ps@gmail.com>
2026-01-15 02:04:12 +0530

    updating monitoring.tf

--- dev/monitoring.tf
+++ dev/monitoring.tf
@@ -1,23 +1,25 @@
 module "ledger_dashboard" {
   source = "../../ps-ledger-infra-modules-tf/modules/monitoring"
 
-  # We pass a template file and inject our project specific variables into it
+  # Pass these directly to the module
+  project_id     = var.project_id
+  dashboard_name = "Financial-Ledger-SRE-Dashboard"
+  lb_ip          = "" 
+
   dashboard_json = templatefile("${path.module}/monitoring-templates/fin-ledger-sre-dashboard.json.tftpl", {
     dashboard_name = "Financial-Ledger-SRE-Dashboard"
     project_id     = var.project_id
-    lb_name        = "ledger-app-lb" # This matches our LB module name
+    lb_name        = "ledger-app-lb"
   })
 }
 
-# 2. The NEW Alerts Call - This handles the Uptime Checks and Policies
+# 2. The NEW Alerts Call
 module "ledger_alerts" {
   source = "../../ps-ledger-infra-modules-tf/modules/monitoring"
 
   project_id     = var.project_id
   lb_ip          = module.ledger_lb.lb_ip
   dashboard_name = "Ledger-Alerts"
-  
-  # We pass an empty string here because this call is NOT for a dashboard
-  # (Requires a small tweak to our module to make dashboard creation optional)
+
   dashboard_json = "" 
 }
