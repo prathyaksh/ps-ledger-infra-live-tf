393afaed389d50219e6839a7834c80a9d1503cc5
prathyaksh <prathyaksh.ps@gmail.com>
2026-01-26 22:10:54 +0530

    updating LB

--- dev/lb.tf
+++ dev/lb.tf
@@ -1,7 +1,7 @@
 module "ledger_lb" {
   source            = "../../ps-ledger-infra-modules-tf/modules/lb"
   name              = "ledger-app-lb"
-  backend_groups    = [module.ledger_mig.instance_group_self_link]
+  backend_groups    = []
   backend_port_name = "http" # This MUST match the name used in uig.tf
   lb_port           = "80"
 }
