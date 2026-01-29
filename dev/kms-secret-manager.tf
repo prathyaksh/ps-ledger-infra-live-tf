5ab6caa478a5b51956d531f6603bb14f688dd286
prathyaksh <prathyaksh.ps@gmail.com>
2026-01-30 01:26:51 +0530

    updated kms-sec-mgr

--- dev/kms-secret-manager.tf
+++ dev/kms-secret-manager.tf
@@ -1,12 +1,10 @@
-# 1. The Key Ring & Crypto Key (KMS)
 module "kms" {
-  source = "../../ps-ledger-infra-modules-tf/modules/kms"
-  name   = "ledger"
+  source      = "../../ps-ledger-infra-modules-tf/modules/kms"
+  name        = "ledger"
   db_key_name = "ledger-db-key"
-  region = var.region
+  region      = var.region
 }
 
-# 2. The Secret Container (Secret Manager)
 module "secrets" {
   source     = "../../ps-ledger-infra-modules-tf/modules/secret-manager"
   name       = "ledger"
